51cd6f34ec2e93f54ced741194a5f811fc4c1b2f
antonreznikov <orxtxega13@gmail.com>
2016-08-04 12:29:35 +0300

    Added chromedriver for mac to project folder

--- BaseTest.py
+++ BaseTest.py
@@ -1,11 +1,16 @@
 import unittest
+import os
 
 from selenium import webdriver
 
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        chromedriver = dir_path + "/webdriver/mac/2.22/chromedriver"
+        print chromedriver
+        os.environ["webdriver.chrome.driver"] = chromedriver
+        self.driver = webdriver.Chrome(chromedriver)
 
     def tearDown(self):
         self.driver.close()
