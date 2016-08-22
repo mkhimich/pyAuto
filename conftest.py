7e655d642e92ff7eb95097d21da3c7867e998774
antonreznikov <orxtxega13@gmail.com>
2016-08-22 13:33:55 +0300

    Added collecting results

--- conftest.py
+++ conftest.py
@@ -189,15 +189,3 @@ def setup(request):
     request.addfinalizer(tear_down)
 
     return factory
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    # execute all other hooks to obtain the report object
-    outcome = yield
-    rep = outcome.get_result()
-
-    # set an report attribute for each phase of a call, which can
-    # be "setup", "call", "teardown"
-
-    setattr(item, "rep_" + rep.when, rep)
